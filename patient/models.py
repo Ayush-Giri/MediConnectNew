e493a16abf6768f571119d599addac040376328e
Ayush Giri <ayushgiri56@gmail.com>
2026-05-13 11:00:29 +0545

    make patient app and model

---
+++ patient/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from insurance_provider.models import InsuranceProvider
+
+# Create your models here.
+
+User = get_user_model()
+
+
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField()
+    blood_group = models.CharField(max_length=5)
+    allergies = models.TextField()
+    chronic_conditions = models.TextField()
+    insurance_provider = models.ForeignKey(InsuranceProvider, on_delete=models.SET_NULL, null=True)
+    emergency_contact = models.BigIntegerField()
+
+
+
+
+
+
+
